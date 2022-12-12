e6290f4630e9c5b738ee0ac6d65ad60b6f4bb370
Atiqule Islam <manishankarvakta@egmail.com>
2022-12-12 18:12:53 +0600

    update products

--- models/productModel.js
+++ models/productModel.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const productSchema = mongoose.Schema(
-    {
-        name: { type: String, require: true },
-        details: { type: String, require: true },
-        category: { type: String, require: true },
-        unit: { type: String, require: true },
-        photo: { type: String },
-        status: { type: String, enum: ["active", "suspend"] },
-    },
-    {
-        timestamps: true,
-    }
+  {
+    name: { type: String, require: true },
+    details: { type: String, require: true },
+    category: { type: mongoose.Types.ObjectId, ref: "Category", require: true },
+    unit: { type: mongoose.Types.ObjectId, ref: "Unit", require: true },
+    photo: { type: String },
+    status: { type: String, enum: ["active", "suspend"] },
+  },
+  {
+    timestamps: true,
+  }
 );
-const product = new mongoose.model("product", productSchema);
-module.exports = product;
+const Product = new mongoose.model("Product", productSchema);
+module.exports = Product;
