a447bacc6cfea88408a014b58c0408a390cb4b35
rahat97 <iamrahat13@gmail.com>
2022-12-28 11:54:53 +0600

    subContractor added and done

--- models/subContractorModel.js
+++ models/subContractorModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const subContractorSchema = mongoose.Schema(
     {
-        name: { type: String, require: true },
-        phone: { type: String, require: true },
-        email: { type: String, require: true },
-        address: { type: String, require: true },
-        nid: { type: String, require: true },
+        name: { type: String },
+        phone: { type: String },
+        email: { type: String },
+        address: { type: String },
+        nid: { type: String },
         status: { type: String, enum: ["active", "suspend"] },
     },
     {
